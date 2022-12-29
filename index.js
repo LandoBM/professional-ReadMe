bf5e35dcbb918a94e571f223c0b0381204ff3b7e
Landon Murray <landonbmurray@icloud.com>
2022-12-28 23:59:55 -0500

    added js to generateMardown and index

--- index.js
+++ index.js
@@ -2,84 +2,111 @@
 const inquirer = require('inquirer')
 const fs = require('fs');
 const { default: test } = require('node:test');
-const generateMarkdown = require('./utils/generateMarkdown.js')
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const { userInfo } = require('os');
+
 // TODO: Create an array of questions for user input
-const questions = [];
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is the title of the project?',
-            name: 'title',
-        },
-        {
-            type: 'input',
-            message: 'How would you describe the project?',
-            name: 'Description',
-        },
-        {
-            type: 'input',
-            message: 'How do I install the project?',
-            name: 'install',
-        }, 
-        {
-            type: 'input',
-            message: 'How to use this project?',
-            name: 'usageinfo',
-        }, 
-        {
-            type: 'input',
-            message: 'Who have contributed to this project?',
-            name: 'contribution',
-        }, 
-        {
-            type: 'list',
-            list: 'Which license was used?',
-            name: 'License',
-            choices:['N/A','MIT License', 'GPL License', 'Apache License', 'GNU License']
-        }, 
-        {
-            type: 'input',
-            message: 'GitHub Username',
-            name: 'githubuser',
-        }, 
-        {
-            type: 'input',
-            message: 'E-mail',
-            name: 'email',
-        }, 
-    ]
-).then(({title, description, install, usageinfo, contribution, license, test, githubuser, email})=> {
-    // Format for ReadME
-    const model = `# ${title}
-
-    * [Description](#description)
-    * [Installation](#install)
-    * [Usage](#usageinfo)
-    * [Contribution](#contribution)
-    * [License](#license)
-    * [Test](#test)
-    * [Questions](#questions)
-    # Description
-    ${description}
-    ## Installation
-    ${install}
-    ## Usage
-    ${usageinfo}
-    ## Contribution
-    ${contribution}
-    ## License
-    ${license}
-    ## Test
-    ${test}
-
-    ## Questions
-    * Github :${githubuser}
-    * E-Mail :${email}`
-
-    writeToFile(title, model)
-})
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of the project?',
+        name: 'title',
+        validate: title => {
+            if(title) {
+            return true
+            }else {
+            console.log('Enter a Title for the project'); 
+            return false}
+        }
+    },
+    {
+        type: 'input',
+        message: 'How would you describe the project?',
+        name: 'Description',
+        validate: description => {
+            if(description) {
+            return true
+            }else {
+            console.log('Enter a description for the project'); 
+            return false}
+        }
+    },
+    {
+        type: 'input',
+        message: 'How do I install the project?',
+        name: 'install',
+        validate: install => {
+            if(install) {
+            return true
+            }else {
+            console.log('Enter instructions on installs'); 
+            return false}
+        }
+    }, 
+    {
+        type: 'input',
+        message: 'How to use this project?',
+        name: 'usageinfo',
+        validate: userInfo => {
+            if(userInfo) {
+            return true
+            }else {
+            console.log('Enter how to use the project'); 
+            return false}
+        }
+    }, 
+    {
+        type: 'input',
+        message: 'Who have contributed to this project?',
+        name: 'contribution',
+        validate: contribution => {
+            if(contribution) {
+            return true
+            }else {
+            console.log('Enter all whom contributed to the project'); 
+            return false}
+        }
+    }, 
+    {
+        type: 'list',
+        list: 'Which license was used?',
+        name: 'License',
+        choices:['N/A','MIT License', 'GPL License', 'Apache License', 'GNU License'],
+        validate: license => {
+            if(license) {
+            return true
+            }else {
+            console.log('Enter a Title for the project'); 
+            return false}
+        }
+    }, 
+    {
+        type: 'input',
+        message: 'GitHub Username',
+        name: 'githubuser',
+        validate: githubuser => {
+            if(githubuser) {
+            return true
+            }else {
+            console.log('Must enter your GitHub Username'); 
+            return false}
+        }
+    }, 
+    {
+        type: 'input',
+        message: 'E-mail',
+        name: 'email',
+        validate: email => {
+            if(email) {
+            return true
+            }else {
+            console.log('Enter an E-mail to proceed'); 
+            return false}
+        }
+    }, 
+]
 // TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
 fs.writeFile(`./${fileName.toLowerCase().split(' ').join('').md}`,data, (error)=>{
    if(error){
@@ -90,8 +117,28 @@ fs.writeFile(`./${fileName.toLowerCase().split(' ').join('').md}`,data, (error)=
 })
 }
 
+// function writeToFile(fileName,data){
+//     return new Promise((resolve, reject) => {
+//         fs.writeFile(`./${fileName}.toLowerCase().split(' ').join('').md`,data,(err)=>{
+//             if(err)
+//             reject(err)
+//             return
+//         })
+//     })
+// }
+
+// function writeToFile(fileName, data){
+//     return fs.writeFile(path.join(process.cwd(), fileName), data)
+// }
+
 // TODO: Create a function to initialize app
-// function init() {}
+function init() {
+    inquirer.prompt(questions)
+        .then(function(response){
+            console.log(response)
+        writeToFile(generateMarkdown({...response}))
+        })
+}
 
 // Function call to initialize app
-// init();
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,69 @@
-const fs = require('fs')
-const inquirer = require('inquirer')
-const index = require('../index.js')
+// const fs = require('fs')
+// const inquirer = require('inquirer')
+// const index = require('../index.js')
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'none') {
+    return ''
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'none') {
+    return `\n* [license](#license)\n`
+  }
+  return ''
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'none') {
+    return `##License`
+  }
+  return ''
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#install)
+  * [Usage](#usageinfo)
+  * [Contribution](#contribution)
+  * [License](#license)
+  * [Test](#test)
+  * [Questions](#questions)
+  ${renderLicenseLink(data.license)}
+    
+  # Description
+  ${data.description}
+  ## Installation
+  ${data.install}
+  ## Usage
+  ${data.usageinfo}
+  ## Contributors
+  ${data.contribution}
+  ## License
+  ${data.license}
+  ## Test
+  ${data.test}
 
-`;
+  ## Questions
+  * Github : [${data.githubuser}](https://github.com/${data.githubuser})
+  * E-Mail :${data.email}
+
+  ${renderLicenseSection(data.license)}
+
+  // writeToFile(title, model)
+  `;
 }
 
 module.exports = generateMarkdown;
